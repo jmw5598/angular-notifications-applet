04a74b1ea913829384b8e20ea5470e91516a5217
Jason White <jmw5598@gmail.com>
2018-08-05 20:01:07 -0400

    added notification panel toggle functionality

--- src/app/notifications-applet/notification-applet.service.ts
+++ src/app/notifications-applet/notification-applet.service.ts
@@ -2,20 +2,32 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { Notification } from './notification.model';
+import { NotificationPanelState } from './notification-panel/notification-panel-state.enum';
 
 @Injectable()
 export class NotificationAppletService {
 
   private notificationsData: Notification[] = [];
+  private panelStateData: NotificationPanelState = NotificationPanelState.CLOSE;
+
   private notificationsSource: BehaviorSubject<Notification[]> =
       new BehaviorSubject<Notification[]>(this.notificationsData);
+  private panelStateSource: BehaviorSubject<NotificationPanelState> =
+      new BehaviorSubject<NotificationPanelState>(this.panelStateData);
 
   public notification = this.notificationsSource.asObservable();
+  public panelState = this.panelStateSource.asObservable();
 
   constructor() { }
 
   toggle() {
-
+    if(this.panelStateData = NotificationPanelState.OPEN) {
+      this.panelStateData = NotificationPanelState.CLOSE;
+      this.panelStateSource.next(this.panelStateData);
+    } else {
+      this.panelStateData = NotificationPanelState.OPEN;
+      this.panelStateSource.next(this.panelStateData);
+    }
   }
 
   add(notification: Notification) {
