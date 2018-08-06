07a144781ab209992ee7c32a03d5af6ad7514acd
Jason White <jmw5598@gmail.com>
2018-08-06 08:35:21 -0400

    created notification indicator template, css, and component logic

--- src/app/notifications-applet/notification-indicator/notification-indicator.component.ts
+++ src/app/notifications-applet/notification-indicator/notification-indicator.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { NotificationAppletService } from '../notification-applet.service';
 
 @Component({
   selector: 'app-notification-indicator',
   templateUrl: './notification-indicator.component.html',
   styleUrls: ['./notification-indicator.component.css']
 })
-export class NotificationIndicatorComponent implements OnInit {
+export class NotificationIndicatorComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subscription: Subscription;
+  count: number = 0;
+
+  constructor(
+    private notificationAppletService: NotificationAppletService
+  ) { }
 
   ngOnInit() {
+    this.subscription = this.notificationAppletService.notifications
+      .subscribe(
+        notifications => this.count = notifications.length
+      );
+  }
+
+  toggle() {
+    this.notificationAppletService.toggle();
+  }
+
+  ngOnDestroy() {
+    if(this.subscription)
+      this.subscription.unsubscribe();
   }
 
 }
