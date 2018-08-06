89756a52b27c5c193d7caad4b26bc547506f405d
Jason White <jmw5598@gmail.com>
2018-08-06 12:26:48 -0400

    updated mock data

--- src/app/notifications.mock.ts
+++ src/app/notifications.mock.ts
@@ -4,7 +4,7 @@ import { NotificationLevel } from './notifications-applet/notification-level.enu
 export const MOCK_NOTIFICATIONS: Notification[] = [
   new Notification(1, 'Subject 1', 'Body 1'),
   new Notification(2, 'Subject 2', 'Body 2', NotificationLevel.SUCCESS),
-  new Notification(2, 'Subject 3', 'Body 3', NotificationLevel.INFO),
-  new Notification(2, 'Subject 4', 'Body 4', NotificationLevel.WARNING),
-  new Notification(2, 'Subject 5', 'Body 5', NotificationLevel.DANGER)
+  new Notification(3, 'Subject 3', 'Body 3', NotificationLevel.INFO),
+  new Notification(4, 'Subject 4', 'Body 4', NotificationLevel.WARNING),
+  new Notification(5, 'Subject 5', 'Body 5', NotificationLevel.DANGER)
 ];
