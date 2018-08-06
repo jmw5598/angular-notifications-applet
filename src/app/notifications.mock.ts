0707ed213b6c73de592982b032b1d8b29fa59217
Jason White <jmw5598@gmail.com>
2018-08-06 11:01:15 -0400

    created mock init data

---
+++ src/app/notifications.mock.ts
@@ -0,0 +1,10 @@
+import { Notification } from './notifications-applet/notification.model';
+import { NotificationLevel } from './notifications-applet/notification-level.enum';
+
+export const MOCK_NOTIFICATIONS: Notification[] = [
+  new Notification(1, 'Subject 1', 'Body 1'),
+  new Notification(2, 'Subject 2', 'Body 2', NotificationLevel.SUCCESS),
+  new Notification(2, 'Subject 3', 'Body 3', NotificationLevel.INFO),
+  new Notification(2, 'Subject 4', 'Body 4', NotificationLevel.WARNING),
+  new Notification(2, 'Subject 5', 'Body 5', NotificationLevel.DANGER)
+];
