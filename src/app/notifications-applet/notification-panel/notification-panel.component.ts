c1f1b3914fe3c89d0f99e468dfd0e6eb6f361471
Jason White <jmw5598@gmail.com>
2018-08-06 11:00:57 -0400

    created notification panel template, css, and component logic

--- src/app/notifications-applet/notification-panel/notification-panel.component.ts
+++ src/app/notifications-applet/notification-panel/notification-panel.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { NotificationAppletService } from '../notification-applet.service';
+import { Notification } from '../notification.model';
+import { NotificationPanelState } from './notification-panel-state.enum';
 
 @Component({
   selector: 'app-notification-panel',
   templateUrl: './notification-panel.component.html',
   styleUrls: ['./notification-panel.component.css']
 })
-export class NotificationPanelComponent implements OnInit {
+export class NotificationPanelComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private notificationsSubscription: Subscription;
+  private panelStateSubscription: Subscription;
+
+  notifications: Notification[];
+  panelState: NotificationPanelState;
+
+  constructor(
+    private notificationAppletService: NotificationAppletService
+  ) { }
 
   ngOnInit() {
+    this.notificationsSubscription = this.notificationAppletService.notifications
+      .subscribe(notifications =>  this.notifications = notifications);
+    this.panelStateSubscription = this.notificationAppletService.panelState
+      .subscribe(panelState => this.panelState = panelState);
+  }
+
+  ngOnDestroy() {
+    if(this.notificationsSubscription)
+      this.notificationsSubscription.unsubscribe();
+    if(this.panelStateSubscription)
+      this.panelStateSubscription.unsubscribe();
   }
 
 }
